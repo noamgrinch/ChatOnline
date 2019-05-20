45191e106ed7829de0431d27e1741b6194da5d6b
noamgrinch <31648669+noamgrinch@users.noreply.github.com>
2019-05-20 22:28:55 +0300

    Update RunServers.java

--- Server/RunServers.java
+++ Server/RunServers.java
@@ -1,14 +1,16 @@
 package Server;
 
-//import CentralLogger.CentralLogger;
+import CentralLogger.CentralLogger;
 
-/*public class RunServers {
+public class RunServers {
 	
 	public static void main(String[] args){
 		Server s = new Server(8888);
 		CentralLogger cl = new CentralLogger();
-		s.run();
-		cl.run();
+		Thread server = new Thread(s);
+		Thread centrallogger = new Thread(cl);
+		server.start();
+		centrallogger.start();
 	}
 
-}*/
+}
